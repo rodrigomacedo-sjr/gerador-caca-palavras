45236bf117ec5b391de14ca102120119b413f74d
roger <126273948+rodrigomacedo-sjr@users.noreply.github.com>
2023-09-07 13:30:27 -0300

    cpalavras.c
    gustavo games

--- cpalavras.c
+++ cpalavras.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main(void)
-{
-    // vamo fazer um caca palavras
+int main() {
+    int linhas, colunas;
+
+    printf("Digite o número de linhas da matriz: ");
+    scanf("%d", &linhas);
+    printf("Digite o número de colunas da matriz: ");
+    scanf("%d", &colunas);
+
+    if (linhas <= 0 || colunas <= 0) {
+        printf("Número inválido de linhas ou colunas.\n");
+    }
+    else {
+        char matriz[linhas][colunas];
+
+        char letra = 'A';
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matriz[i][j] = letra;
+            }
+        }
+
+        printf("Matriz gerada:\n");
+        
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                printf("%c ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
 }
